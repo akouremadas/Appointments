6e059da9365c1a7564a6e06fe35f4eb9a6ab04d3
akouremadas <kouremagas@gmail.com>
2018-11-20 00:36:36 +0200

    #6 Add on project creation "Admin" role and an admin user.

--- Appointments.Domain/Migrations/DbMigrationsConfig.cs
+++ Appointments.Domain/Migrations/DbMigrationsConfig.cs
@@ -1,9 +1,14 @@
 namespace Appointments.Domain.Migrations
 {
+    using Appointments.Domain.Entities;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web;
 
     public sealed class DbMigrationsConfig : DbMigrationsConfiguration<Appointments.Domain.Entities.ApplicationDbContext>
     {
@@ -13,12 +18,37 @@ namespace Appointments.Domain.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(Appointments.Domain.Entities.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            const string name = "admin@admin.com";
+            const string password = "Admin@123456";
+            const string roleName = "Admin";
+
+
+            if (!context.Roles.Any(r => r.Name == roleName))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = roleName };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == name))
+            {
+
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser {UserName = name, Email = name };
+
+                manager.Create(user, password) ;
+                manager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
