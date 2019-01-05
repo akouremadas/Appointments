16152deff577d2b554ffa8c5f78490aed326ef24
akouremadas <kouremagas@gmail.com>
2019-01-05 21:54:15 +0200

    #20 Adding Reporting Pages

---
+++ Appointments.WebUI/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Appointments.Domain.Entities;
+using Appointments.WebUI.Models;
+
+namespace Appointments.WebUI.Controllers
+{
+    public class ReportController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Report
+        public ActionResult AppointmentReport()
+        {
+            var appointments = db.Appointments.Include(a => a.Client).Include(a => a.Result);
+            return View(appointments.ToList());
+        }
+
+        public ActionResult ClientReport()
+        {
+            return View(db.Clients.ToList());
+        }
+    }
+}
